2b82a9a239209b917ca71597b454ed983c2c5bdf
morn1ngstar <alexmorningstar581@yandex.by>
2022-08-04 19:36:48 +0300

    make root colors on css, start creating dark-mode

--- app/js/main.js
+++ app/js/main.js
@@ -23,9 +23,39 @@ new Swiper('.swiper', {
   spaceBetween: 20,
   speed: 1000,
   effect: 'fade',
-  modules:[Autoplay, EffectFade],
-  autoplay:{
+  modules: [Autoplay, EffectFade],
+  autoplay: {
     delay: 5000,
     waitForTransition: true,
   },
 })
+
+
+// dark-mode
+
+const htmlElement = document.documentElement;
+const themeToggle = document.getElementById('dat-checkbox');
+
+themeToggle.addEventListener('change', function () {
+
+    theme()
+
+});
+
+
+function theme() {
+
+  if (themeToggle.checked) {
+    htmlElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark')
+  } else {
+    htmlElement.removeAttribute('data-theme');
+    localStorage.removeItem('theme');
+
+  }
+  if (localStorage.getItem('theme') !== null){
+    htmlElement.setAttribute('data-theme', 'dark')
+  }
+}
+theme();
+
